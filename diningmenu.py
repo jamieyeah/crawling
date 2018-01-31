e8e63a6d123c2b288eeb8d72877a11aed793d0af
jamie <sujin.jung.424@gmail.com>
2018-01-31 22:45:31 +0900

    Create diningmenu.py
    crawling of the dining list

---
+++ diningmenu.py
@@ -0,0 +1,23 @@
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+html=urlopen('https://dorm.chungbuk.ac.kr/sub05/5_2_tab2.php?type1=5&type2=2')
+source=html.read()
+html.close()
+
+
+soup=BeautifulSoup(source,"lxml")
+print(soup.prettify())
+div_contents = soup.find("div","contents")
+
+print(div_contents.prettify())
+
+table = div_contents.find_all("table")
+print(table[0])
+menu_table=table[0]
+trs=menu_table.find_all('tr')
+
+sun_trs=trs[1]
+sun_tds=sun_trs.find_all('td')
+sun_mon=sun_tds[1].get_text(strip=True)
